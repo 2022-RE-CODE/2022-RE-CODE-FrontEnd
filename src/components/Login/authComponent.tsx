ce286cd53162eaee3135a0b7d2055bcfe0383d36
youngmin <min050410@gmail.com>
2022-10-11 19:18:42 +0900

    add: Auth Page

--- src/components/Login/authComponent.tsx
+++ src/components/Login/authComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
-import '../../styles/login.css';
+import '../../styles/auth.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { instance } from '../api/axios.instance';
 import { instanceWithHeader } from '../api/axiosWithHeader.instance';
@@ -97,23 +97,29 @@ const AuthComponent: React.FC = () => {
                 <Link to="/" className="Header--home-btn">
                     <img className="Header--logo-img" src={'logo-recode.png'} />
                 </Link>
+                <div className="login--title1">자신의 능력으로</div>
+                <div className="login--title2">추가 수익을 만들어보세요!</div>
+
                 <form className="auth--form" onSubmit={submitHandler}>
-                    <input type="email" value={email} onChange={emailHandler} placeholder="이메일"></input>
-                    <button type="button" onClick={getVerificationCode}>인증메일 발송</button>
+                    <div className="auth--email-container">
+                        <input type="email" value={email} onChange={emailHandler} placeholder="이메일"></input>
+                        <button className="auth--form--emailBtn" type="button" onClick={getVerificationCode}>인증메일 발송</button>
+                    </div>
                     <input value={verificationCode} onChange={codeHandler} placeholder="이메일 인증코드"></input>
                     <input value={nickname} onChange={nicknameHandler} placeholder="닉네임"></input>
                     <input type="password" value={password} onChange={passwordHandler} placeholder="비밀번호"></input>
                     <input type="password" value={checkPassword} onChange={checkPasswordHandler} placeholder="비밀번호 확인"></input>
-                    <select value={position} onChange={positionHandler}>
+                    <select className="auth--form-position" value={position} onChange={positionHandler}>
                         {Object.keys(EnumPosition).map((key: string) => (
                             <option key={key} value={key}>
                                 {EnumPosition[key]}
                             </option>
                         ))}
                     </select>
-                    <button type="submit">회원가입하기</button>
-                    <button><Link to="/login">돌아가기</Link></button>
+                    <button className="auth--form--submitBtn" type="submit">회원가입하기</button>
                 </form>
+
+                
             </div>
         </div>
     )
