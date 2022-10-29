dbc49eca83b33060cdc3fd7b8793bd860b22b82d
youngmin <min050410@gmail.com>
2022-10-29 23:55:16 +0900

    add: Redux Theme action

---
+++ src/redux/theme/action/theme.action.ts
@@ -0,0 +1,4 @@
+import { DARK_MODE, WHITE_MODE } from "./theme.actionType";
+
+export const darkMode = () => ({type: DARK_MODE});
+export const whiteMode = () => ({type: WHITE_MODE});
---
+++ src/redux/theme/action/theme.actionType.ts
@@ -0,0 +1,2 @@
+export const DARK_MODE = 'theme/darkmode' as const;
+export const WHITE_MODE = 'theme/whitemode' as const;
