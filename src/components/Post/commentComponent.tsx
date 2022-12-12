c22657c4f30ff75c0ebf18b3a96e8d44ce7f5b27
youngmin <min050410@gmail.com>
2022-12-12 20:37:43 +0900

    add: 댓글 표시 및 생성

--- src/components/Post/commentComponent.tsx
+++ src/components/Post/commentComponent.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 import { UserType } from '../../redux/user/reducer/user.reducerType';
 import '../../styles/comment.css';
+import instanceWithToken from '../api/axiosWithToken.instance';
 import { CategoryType, CommentType } from './postType';
 
 type CommentComponentType = {
@@ -20,15 +22,47 @@ type CommentComponentType = {
 const CommentComponent: React.FC<CommentComponentType> = ({
     postInfo
 }: CommentComponentType) => {
+
+    const [comment, setComment] = useState("");
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const commentHandler = (e: any) => {
+        e.preventDefault();
+        setComment(e.target.value);
+    };
+
+    const reset = () => {
+        setComment("");
+    }
+
+    const submitHandler = async () => {
+        await instanceWithToken.post(`/comment/create/${id}`, JSON.stringify({
+            comment: comment
+        }));
+        window.location.reload();
+    }
+
     return (
         <div className='post--info--comment-container'>
-            {postInfo?.comments.map((comment: CommentType) => {
-                return (
-                    <div className='post--comment'>
-                        {comment.nickname} {comment.comment}
-                    </div>
-                )
-            })}
+            <div className='post--info--comment--title'>피드백 전체보기</div>
+            <div className='post--info--comment-all'>
+                {postInfo?.comments.map((comment: CommentType) => {
+                    return (
+                        <div className='post--comment'>
+                            <div className='post--comment-nickname'>{comment.nickname}님의 피드백</div>
+                            <div className='post--comment-content'>{comment.comment}</div>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className='post--info--comment-form'>
+                <textarea placeholder='피드백 추가하기' onChange={commentHandler} value={comment}></textarea>
+                <div className='post--info--comment--button-wrapper'>
+                    <button className='post--info--comment-resetBtn' onClick={reset}>취소</button>
+                    <button className='post--info--comment-submitBtn' onClick={submitHandler}>확인</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/container/postInfoContainer/PostInfoContainer.tsx
+++ src/container/postInfoContainer/PostInfoContainer.tsx
@@ -59,6 +59,7 @@ export const PostInfoContainer = () => {
                 postInfo={postInfo}
                 ToggleLikes={ToggleLikes}
             />
+            <FooterComponent />
         </div>
     )
 }
