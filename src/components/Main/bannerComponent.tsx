1ad3bbb4e23fed89f4a0ba9b7f84d85bbf729096
youngmin <min050410@gmail.com>
2022-12-14 17:12:15 +0900

    add: 배너 Interval 애니메이션

--- src/components/Main/bannerComponent.tsx
+++ src/components/Main/bannerComponent.tsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import '../../styles/banner.css';
+import useInterval from '../../utils/useInterval';
 
 const BannerComponent: React.FC = () => {
 
+    const bannerTitle = useMemo(() => {
+        return "LEVEL UP YOUR CODES";
+      }, []);
+
+    const [landingTitle, setLandingTitle] = useState("");
+    const [isIncreasing, setIsIncreasing] = useState(true);
+    const [count, setCount] = useState(0);
     const navigate = useNavigate();
+
+    useInterval(() => {
+        if (count >= bannerTitle.length) {
+            setIsIncreasing(false);
+        }
+        if (count <= 2) {
+            setIsIncreasing(true);
+        }
+        setLandingTitle((prev: string) => {
+          let result = "";
+          if (isIncreasing) {
+            result = prev ? bannerTitle.slice(0, count) : bannerTitle[0];
+            setCount((prev) => prev + 1);
+          }
+          else {
+            result = prev ? bannerTitle.slice(0, count) : bannerTitle[0];
+            setCount((prev) => prev - 1);
+          }
+          return result;
+        });
+    }, 250);
 
     return (
         <div className="banner">
             <div className="banner--container">
                 <div className="banner--container-firstline">
-                     RE: CODE
+                    RE: CODE
                 </div>
                 <div className="banner--container-secondline">
-                    LEVEL UP YOUR CODES
+                    {landingTitle}
                 </div>
                 <button className="btn-start-now" onClick={()=>navigate('/login')}>지금 시작하기</button>
             </div>
